3cbaa3953dbaa4a91baa644a7357ec41b57202e5
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-07-25 22:55:49 +0330

    create componentPage route style and file edit article done

---
+++ src/pages/editArticle/EditArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './EditArticle.css'
+const EditArticle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EditArticle;
