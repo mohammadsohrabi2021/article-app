d63a70f2744045da28efd26fa1f23f89d7d157b2
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-07-25 22:55:06 +0330

    create componentPage route style and file addArticle done

---
+++ src/pages/addArticle/AddArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AddArticle.css'
+const AddArticle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddArticle;
