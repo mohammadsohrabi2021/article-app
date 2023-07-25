890fc6bc0320078c3dac717f13684e470a8dfa3b
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-07-25 22:53:33 +0330

    create component style and file articleItem done

---
+++ src/components/article/ArticleItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ArticleItem.css'
+const ArticleItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ArticleItem;
