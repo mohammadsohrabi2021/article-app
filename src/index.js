c24c77028ddd854e70941c436626299a776694eb
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-07-26 01:16:02 +0330

    refactor index.js done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import { ThemeProvider } from "react-bootstrap";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider dir="rtl">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
