a07ddb2af1911b617629f24760aee6a8035c708c
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-01 19:37:34 +0330

    refactor component app done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AddArticle from "./pages/addArticle/AddArticle";
+import Article from "./pages/article/Article";
+import EditArticle from "./pages/editArticle/EditArticle";
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/add-article" element={<AddArticle />} />
+        <Route path="/article/:articleId" element={<Article />} />
+        <Route path="/edit-article/:articleId" element={<EditArticle />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
