29be624fb2f17ac9a5872153d80aac6202d9e6ea
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-01 19:34:44 +0330

    refactor component aboutPage done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MyNavbar from '../../components/navbar/MyNavbar';
 import './About.css'
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <MyNavbar/>        
+        </>
     );
 };
 
